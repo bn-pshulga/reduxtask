ade58e1d797d613d5595a420218f8f760bee5a2e
Pavel Shulga <pavel.shulga@bndigital.co>
2022-06-20 18:21:08 +0300

    initial commit

--- src/features/counter/counterSlice.spec.ts
+++ src/features/counter/counterSlice.spec.ts
@@ -2,18 +2,18 @@ import counterReducer, {
   CounterState,
   increment,
   decrement,
-  incrementByAmount,
+  // incrementByAmount,
 } from './counterSlice';
 
 describe('counter reducer', () => {
   const initialState: CounterState = {
     value: 3,
-    status: 'idle',
+    // status: 'idle',
   };
   it('should handle initial state', () => {
     expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
       value: 0,
-      status: 'idle',
+      // status: 'idle',
     });
   });
 
@@ -27,8 +27,8 @@ describe('counter reducer', () => {
     expect(actual.value).toEqual(2);
   });
 
-  it('should handle incrementByAmount', () => {
-    const actual = counterReducer(initialState, incrementByAmount(2));
-    expect(actual.value).toEqual(5);
-  });
+  // it('should handle incrementByAmount', () => {
+  //   const actual = counterReducer(initialState, incrementByAmount(2));
+  //   expect(actual.value).toEqual(5);
+  // });
 });
